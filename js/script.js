9c8c18df2be092ff2c4e414d4b91668ef9f9be40
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-11-19 16:27:54 +0100

    feat: milestone 3 done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const cardContainer = document.getElementById("card-container");
+const cardContainer = document.querySelector("#card-container");
 
 
 
@@ -8,8 +8,8 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/").then((resp) => {
     cardArray.forEach((curCard, index) => {
 
         cardString += `
-            <div class="card-rotate col col-sm-12 col-md-6 col-lg-4 mb-5 ">
-                <div id="cardElem" class="card  position-relative">
+            <div class="card-rotate col col-sm-12 col-md-6 col-lg-4 mb-5">
+                <div class="card position-relative" data-img="${curCard.url}">
                     <div>
                         <img width="10%" class="red-pin position-absolute top-0 start-50 translate-middle" src="./img/pin.svg"
                             alt="">
@@ -28,22 +28,28 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/").then((resp) => {
 
     cardContainer.innerHTML = cardString;
 
-    const card = document.querySelectorAll(".card");
-    const overlay = document.getElementById("overlay-elem");
+    const cards = document.querySelectorAll(".card");
+    const overlay = document.querySelector("#overlay-elem");
+    const imageInOverlay = document.querySelector("#img-overlay");
+    const button = document.querySelector("#button-close");
 
-    card.forEach(card => {
-
-        card.addEventListener("click", function () {
+    cards.forEach(card => {
+        card.addEventListener("click", () => {
+            imageInOverlay.src = card.dataset.img;
             overlay.classList.remove("d-none");
         });
     });
 
-    const button = document.getElementById("button-close");
 
-    button.addEventListener("click", function () {
+    button.addEventListener("click", () => {
         overlay.classList.add("d-none");
     });
 
+
+    // facendo in questo modo scompare tutto l'overlay ma anche cliccando sull'immagine
+    // overlay.addEventListener("click", function () {
+    //     overlay.classList.add("d-none");
+    // });
 });
 
 
