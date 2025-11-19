18b13c7b590f978afd978d7f74ff764f9a355df4
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-11-19 12:41:35 +0100

    feat: inizio milestone 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const cardContainer = document.getElementById("card-container");
-console.log(cardContainer);
+
+
 
 axios.get("https://lanciweb.github.io/demo/api/pictures/").then((resp) => {
     const cardArray = resp.data;
@@ -7,23 +8,33 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/").then((resp) => {
     cardArray.forEach((curCard, index) => {
 
         cardString += `
-            <div class="col col-sm-12 col-md-6 col-lg-4 mb-5 ">
-                <div class="card  position-relative">
+            <div class="card-rotate col col-sm-12 col-md-6 col-lg-4 mb-5 ">
+                <div id="cardElem" class="card  position-relative">
                     <div>
-                        <img width="10%" class="position-absolute top-0 start-50 translate-middle" src="./img/pin.svg"
+                        <img width="10%" class="red-pin position-absolute top-0 start-50 translate-middle" src="./img/pin.svg"
                             alt="">
                     </div>
                     <div class="container mt-4 mb-4 px-4">
-                        <img src="${cardArray[index].url}" class="card-img-top" alt="...">
+                        <img src="${curCard.url}" class="card-img-top" alt="...">
                     </div>
 
                     <div class="card-body">
-                        <p class="card-text"><small class="text-body-secondary">${cardArray[index].date}</small></p>
-                        <h2 class="card-text font-sometype-mono">${cardArray[index].title}</h2>
+                        <p class="card-text"><small class="text-body-secondary">${curCard.date}</small></p>
+                        <h2 class="card-text font-sometype-mono">${curCard.title}</h2>
                     </div>
                 </div>
             </div>`
     });
 
     cardContainer.innerHTML = cardString;
+
+    const card = document.querySelectorAll(".card");
+    console.log(card);
+    const overlay = document.getElementById("overlay-elem");
+
+    card.addEventlistener("click", function () {
+        overlay.classList.remove("d-none");
+    });
+
 });
+
